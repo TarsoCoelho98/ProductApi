6fb754d3a701f4f80dd22ebd05c029c7ce6c7d50
TarsoCoelho98 <tarsocoelho98@gmail.com>
2022-01-22 20:39:27 -0300

    Projeto finalizado

---
+++ ProductAPI/Data/AppContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Data
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(x => x.Name).HasMaxLength(80);
+            modelBuilder.Entity<Product>().Property(x => x.Price).HasPrecision(10, 2);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "PC", Price = 345.45m },
+                new Product { Id = 2, Name = "Note", Price = 312.3m },
+                new Product { Id = 3, Name = "Laptop", Price = 231m });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ ProductAPI/Migrations/20220109035745_Initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(80) CHARACTER SET utf8mb4", maxLength: 80, nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[] { 1, "PC", 345.45m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[] { 2, "Note", 312.3m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[] { 3, "Laptop", 231m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
